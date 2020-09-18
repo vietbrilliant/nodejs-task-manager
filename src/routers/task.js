9c98857f63e8110b875990847a39e53937ab4e7b
Hieu Duong <hieu.duong@vietbrilliant.com>
2020-09-18 12:36:20 -0700

    Add Sorting, Pagination, And Filtering into get tasks API endpoint

--- src/routers/task.js
+++ src/routers/task.js
@@ -14,14 +14,36 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks/completed=true
+// GET /tasks/limit=10&skip=20
+// GET /tasks/sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
   try {
+    const match = {};
+    const sort = {};
+    if (req.query.completed) {
+      match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(':');
+      sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     // Solution 1: Query directly from tasks docs
     // const tasks = await Task.find({ owner: req.user._id });
     // res.send(tasks);
 
     // Solution 2: Query from virtual docs
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate('tasks').execPopulate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort,
+      },
+    });
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
